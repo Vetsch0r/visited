b0683906712880e8a36057f00540f158c33e5af2
Thomas Vetsch <tvetsch89@gmail.com>
2017-12-18 22:31:05 +0100

    add button for bluetooth connection

--- visited/www/js/Controller.js
+++ visited/www/js/Controller.js
@@ -135,6 +135,14 @@ Controller.prototype = {
         controller.takeScreenshot('instagram');
       }, 500);
     });
+    
+    /*
+    * Bluetooth connector
+    */
+    $(document).on('click', "#wallDisplay", function(e) {
+      var macAddress = "98:D3:31:FB:5E:63";
+      bluetoothSerial.connect(macAddress, controller.onBluetoothConnect, controller.onBluetoothDisconnect);
+    });
 
     /*
     * Back button should close the settings panel
@@ -147,6 +155,14 @@ Controller.prototype = {
         navigator.app.backHistory();
       }
     }, false);
+  },
+  
+  onBluetoothConnect: function(){
+    $('#wallDisplay').prev('span.ui-btn-inner').css('color','green');
+  },
+  
+  onBluetoothDisconnect: function(){
+    $('#wallDisplay').prev('span.ui-btn-inner').css('color','red');
   },
 
   takeScreenshot: function(target){
