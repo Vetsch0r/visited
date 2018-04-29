47e64e7fc8f4d2bbc5a6db11ce396f639b6e653a
Thomas Vetsch <tvetsch89@gmail.com>
2018-04-29 12:22:55 +0200

    add gitignore and webpack for import statements

---
+++ visited/platforms/ios/www/js/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <vectormap></vectormap>
+    </div>
+</template>
+<script>
+    import VectorMap from './VectorMap.vue';
+
+    export default {
+        data(){
+            return {
+                
+            }
+        },
+        components: {
+            vectormap: VectorMap
+        }
+    }
+</script>
